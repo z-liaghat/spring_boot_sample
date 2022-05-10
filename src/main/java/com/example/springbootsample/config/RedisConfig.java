7b07733713380d72af0ae358601b22f8bceb7f6c
zliaghat <z.liaghat@gmail.com>
2022-05-10 13:09:25 +0430

    add config for caching with redis

---
+++ src/main/java/com/example/springbootsample/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.example.springbootsample.config;
+
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+}
